4ebcf0cf5373074314d2481f20e2fc0c4440d522
Giovanni <jovincu123@gmail.com>
2023-03-24 10:42:12 -0300

    Aula 11 já finalizada

---
+++ aula_11/exer_08.js
@@ -0,0 +1,8 @@
+//condições simples (if) no OUTPUT do Vs Code com o "F8". Usando o "if"
+var velocidade = 61;
+console.log(`Seu carro esta a ${velocidade} KM/h`);
+if (velocidade > 60) {
+    console.log("Você está multado por excesso de velocidade")
+}
+
+
---
+++ aula_11/exer_09.js
@@ -0,0 +1,8 @@
+//condições composta (if - else) no OUTPUT do Vs Code com o "F8". Usando o "if" e "else"
+var pais = "EUA"
+
+if (pais == "brasil") {
+    console.log("Você é brazuca")
+} else {
+    console.log("Pra gringo é mais caro")
+}
